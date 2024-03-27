2e4ea5f64b6eeaaaf4f975df99fe66ad67915a0c
Lucas Henrique :) <84149138+LukC30@users.noreply.github.com>
2024-03-27 13:53:23 -0300

    fix

--- Produtos.php
+++ Produtos.php
@@ -8,7 +8,7 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <link rel="stylesheet" href="./bootstrap/css/bootstrap.css">
     <link rel="stylesheet" href="css/card.css">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="./css/style.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <style>
         .navbar-brand img {
